caffa19b673a03e0517a753cb158840fdb169be4
ThamerKehail <thamerkehail@outlook.com>
2025-06-01 23:38:57 +0300

    create account

--- lib/presentation/auth/bloc/check_id_state.dart
+++ lib/presentation/auth/bloc/check_id_state.dart
@@ -2,17 +2,28 @@ part of 'check_id_cubit.dart';
 
 @immutable
 sealed class CheckIdState {
+  final bool isValid;
+
+  CheckIdState(this.isValid);
 }
 
-final class CheckIdInitial extends CheckIdState {}
-final class CheckIdLoading extends CheckIdState {}
+final class CheckIdInitial extends CheckIdState {
+  CheckIdInitial() : super(false);
+}
+
+final class CheckIdLoading extends CheckIdState {
+  CheckIdLoading() : super(false);
+}
+
 final class CheckIdLoaded extends CheckIdState {
   final CheckIdModel checkIdModel;
 
-  CheckIdLoaded(this.checkIdModel);
+  CheckIdLoaded(this.checkIdModel)
+    : super((checkIdModel.signUpStatus == "yes"));
 }
+
 final class CheckIdFailure extends CheckIdState {
   final String message;
 
-  CheckIdFailure(this.message);
+  CheckIdFailure(this.message) : super(false);
 }
