75cc14b6afa345e2018e448201cc02c28e049b1f
YaserMelhem23 <yasermelhem60@gmail.com>
2025-06-12 18:30:18 +0300

    test

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -31,76 +31,76 @@ class _HomePageState extends State<HomePage>
             _noAppoints
                 ? NoAppointmentsWidget()
                 : Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Column(
-                        children: <Widget>[
-                          MedicalSearchWidget(),
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 10.0.w),
-                            child: SectionHeaderWidget(
-                              title: 'next_appointment'.tr(),
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 10.0.w),
-                            child: NextAppointmentWidget(),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 10.0.w),
-                            child: SectionHeaderWidget(
-                              title: 'Medical authorities'.tr(),
-                              onPressed: () => Navigator.of(
-                                context,
-                              ).pushNamed(Routes.myDoctors),
-                            ),
-                          ),
-                        ],
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    MedicalSearchWidget(),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10.0.w),
+                      child: SectionHeaderWidget(
+                        title: 'next_appointment'.tr(),
                       ),
-                      Container(
-                        height: 160,
-                        child: ListView.separated(
-                          separatorBuilder: (context, index) =>
-                              SizedBox(width: 15.w),
-                          itemCount: 4,
-                          scrollDirection: Axis.horizontal,
-                          padding: EdgeInsets.symmetric(horizontal: 20),
-                          itemBuilder: (context, index) {
-                            return MedicalAuthoritiesListItem(
-                              doctor: doctors[index],
-                            );
-                          },
-                        ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10.0.w),
+                      child: NextAppointmentWidget(),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10.0.w),
+                      child: SectionHeaderWidget(
+                        title: 'Medical authorities'.tr(),
+                        onPressed: () => Navigator.of(
+                          context,
+                        ).pushNamed(Routes.myDoctors),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 10.0.w),
-                            child: SectionHeaderWidget(
-                              title: 'medical_centers'.tr(),
-                              onPressed: () {},
-                            ),
-                          ),
-                          Container(
-                            height: 160,
-                            child: ListView.separated(
-                              separatorBuilder: (context, index) =>
-                                  SizedBox(width: 15),
-                              itemCount: 4,
-                              scrollDirection: Axis.horizontal,
-                              padding: EdgeInsets.symmetric(horizontal: 20),
-                              itemBuilder: (context, index) {
-                                return MedicalCentersListItem(
-                                  medicalCenter: medicalCenter[index],
-                                );
-                              },
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+                    ),
+                  ],
+                ),
+                Container(
+                  height: 160,
+                  child: ListView.separated(
+                    separatorBuilder: (context, index) =>
+                        SizedBox(width: 15.w),
+                    itemCount: 4,
+                    scrollDirection: Axis.horizontal,
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    itemBuilder: (context, index) {
+                      return MedicalAuthoritiesListItem(
+                        doctor: doctors[index],
+                      );
+                    },
                   ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10.0.w),
+                      child: SectionHeaderWidget(
+                        title: 'medical_centers'.tr(),
+                        onPressed: () {},
+                      ),
+                    ),
+                    Container(
+                      height: 160,
+                      child: ListView.separated(
+                        separatorBuilder: (context, index) =>
+                            SizedBox(width: 15),
+                        itemCount: 4,
+                        scrollDirection: Axis.horizontal,
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        itemBuilder: (context, index) {
+                          return MedicalCentersListItem(
+                            medicalCenter: medicalCenter[index],
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ],
         ),
       ),
